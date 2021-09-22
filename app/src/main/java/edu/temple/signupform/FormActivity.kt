ccb9ce4b9948b647cfcdfb4b3119a2e7bb31b9fd
APersonTakingClasses <70277494+APersonTakingClasses@users.noreply.github.com>
2021-09-22 00:18:28 -0400

    Problems with error/validation
    The problem is how I'm creating the values for the username,email,password1,password2 onClick and I am not clever enough to scope outside of the onClick for setting the values and the validation

--- app/src/main/java/edu/temple/signupform/FormActivity.kt
+++ app/src/main/java/edu/temple/signupform/FormActivity.kt
@@ -9,6 +9,8 @@ import android.widget.*
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.constraintlayout.widget.ConstraintSet.*
 import android.widget.ArrayAdapter as ArrayAdapter
+import android.text.*
+
 
 class FormActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,35 +25,101 @@ class FormActivity : AppCompatActivity() {
         val password2 = findViewById<EditText>(R.id.password2)
 
         var temp = getText(R.string.greeting)
-        var tempUsername:CharSequence = "123"
-        var tempEmail:CharSequence
-        var tempPassword1:CharSequence
-        var tempPassword2:CharSequence
 
-        val listener = object: View.OnClickListener{
-            override fun onClick(p0: View?) {
-                if(greeting.text != temp){
-                    // button to register has not been clicked once
-                    // set temp save values to user input
-                    tempUsername = username.text
-                    tempEmail = email.text
-                    tempPassword1 = password1.text
-                    tempPassword2 = password2.text
-                    temp = getText(R.string.enterItems)
+
+        var err = "twentee owne"
+        val dur = Toast.LENGTH_SHORT
+
+        val listener = View.OnClickListener {
+            val tempUsername:CharSequence = username.text
+            val tempEmail:CharSequence = "pig"
+            val tempPassword1:CharSequence = "dog"
+            val tempPassword2:CharSequence = "dig"
+
+            if(greeting.text == temp){
+                // button to register has not been clicked once, greeting is still greeting
+                // set temp save values to user input
+                    //done by addTextChangedListener's
+                greeting.setText(R.string.enterItems)
+
+            }
+            else {
+                // temp save values are set, compare current to saved value
+                if(tempUsername.isEmpty()){
+                    err = getString(R.string.errorMessage1) +" "+ getString(R.string.username)
+                    Toast.makeText(applicationContext,err,dur).show()
+                    Log.d("message", err)
                 }
-                else {
-                    // temp save values are set, compare current to saved value
-                    if( tempUsername != username.text){
-                        val toast = Toast.makeText(applicationContext, getText(R.string.errorMessage1), Toast.LENGTH_SHORT)
-                        toast.show()
-                    }
+                if(tempEmail.isEmpty()){
+                    err = getString(R.string.errorMessage1) +" "+ getString(R.string.email)
+                    Toast.makeText(applicationContext,err,dur).show()
+                    Log.d("message", err)
+                }
+                if(tempPassword1.isEmpty()){
+                    err = getString(R.string.errorMessage1) +" "+ getString(R.string.password1)
+                    Toast.makeText(applicationContext,err,dur).show()
+                    Log.d("message", err)
+                }
+                if(tempPassword2.isEmpty()){
+                    err = getString(R.string.errorMessage1) +" "+ getString(R.string.password2)
+                    Toast.makeText(applicationContext,err,dur).show()
+                    Log.d("message", err)
                 }
 
-
+                if(password1.text != password2.text){
+                    err = getString(R.string.errorMessage1) +" "+ getString(R.string.password1) +" "+ getString(R.string.errorMessage2) +" "+ getString(R.string.password2)
+                    Toast.makeText(applicationContext,err,dur).show()
+                    Log.d("message", err)
+                }
             }
         }
 
         rButton.setOnClickListener(listener)
+        username.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+        })
+        email.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+        })
+        password1.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+        })
+        password2.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+        })
     }
 
 
